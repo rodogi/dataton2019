354f4bf1fd444e49fa3fd6582ab63458e7babadf
GDJ <guillermodeandajauregui@gmail.com>
2019-12-06 10:54:43 -0600

    added privaTE company s3 data

--- clean_s3.R
+++ clean_s3.R
@@ -34,9 +34,11 @@ MergeName <- function(my_data, my_new_column, ... ){
 s3_edmx <- fromJSON(txt = "data_local/s3-servidores-bulk/servidores_publicos_sancionados_edoMex.json", flatten = TRUE)
 s3_sfp1 <- fromJSON(txt = "data_local/s3-servidores-bulk/servidores_publicos_sancionados_sfp1.json", flatten = TRUE)
 s3_sfp2 <- fromJSON(txt = "data_local/s3-servidores-bulk/servidores_publicos_sancionados_sfp2.json", flatten = TRUE)
-
+s3_part <-fromJSON(txt = "data_local/s3-servidores-bulk/s3-particulares-sfp.json", flatten = TRUE)
 # same columns?
 
+s3_part %>% colnames
+s3_part %>% head
 setdiff(colnames(s3_edmx), colnames(s3_sfp1)) #nope
 
 ## remove large lists from s3_edmx. The info will be lost, but if needed it is still in the JSON file
@@ -49,6 +51,8 @@ s3_edmx <- MergeName(s3_edmx, my_new_column = "nombre_completo", servidorPublico
 s3_sfp1 <- MergeName(s3_sfp1, my_new_column = "nombre_completo", nombres, primerApellido, segundoApellido)
 s3_sfp2 <- MergeName(s3_sfp2, my_new_column = "nombre_completo", nombres, primerApellido, segundoApellido)
 
+#s3_part looks a little different 
+s3_part <- MergeName(s3_part, my_new_column = "nombre_completo", responsableSancion.nombres, responsableSancion.primerApellido, responsableSancion.segundoApellido)
 
 
 # make names all caps, remove accents 
@@ -68,20 +72,27 @@ s3_sfp2 <-   mutate(.data = s3_sfp2,
                     )
 )
 
+s3_part <-   mutate(.data = s3_part, 
+                    nombre_completo = str_to_upper(stringi::stri_trans_general(nombre_completo, "Latin-ASCII")
+                    )
+)
 #join into single large data frame 
 
 
 #For some reason multa.monto raises "can't convert to integer" error when binding.
 #As it is uninformative (montos are all 0 or NA), we convert manually
-s3_edmx <- mutate(s3_edmx, multa.monto = as.integer(multa.monto))
-s3_sfp1 <- mutate(s3_sfp1, multa.monto = as.integer(multa.monto))
-s3_sfp2 <- mutate(s3_sfp2, multa.monto = as.integer(multa.monto))
+s3_edmx <- mutate(s3_edmx, multa.monto = as.numeric(multa.monto))
+s3_sfp1 <- mutate(s3_sfp1, multa.monto = as.numeric(multa.monto))
+s3_sfp2 <- mutate(s3_sfp2, multa.monto = as.numeric(multa.monto))
+s3_part <- mutate(s3_part, multa.monto = as.numeric(multa.monto))
 
 s3_df <- 
 bind_rows(list(sfp1 = s3_sfp1, 
           sfp2 = s3_sfp2, 
-          edmx = s3_edmx),
+          edmx = s3_edmx,
+          part = s3_part),
           .id = "sistema") 
 
 #write out
 write_delim(x = s3_df, path = "results/s3_bound.txt", delim = "\t", col_names = TRUE)
+
